e2990823988794b69c8f34c7d885860eea6c0aa7
Johannes Dyson <smgjohannes@gmail.com>
2024-10-17 02:28:49 +0200

    another change

--- app/express/validations/propertyValidation.js
+++ app/express/validations/propertyValidation.js
@@ -31,11 +31,11 @@ exports.createSchema = (req, res, next) => {
     image: Joi.any().optional(),
     additionalImages: Joi.any().optional(),
     property_type: Joi.string().valid('rent', 'buy', 'sell').required(),
-    house_number: Joi.string().optional().allow(null),
-    street_name: Joi.string().optional().allow(null),
-    suburb: Joi.string().optional().allow(null),
-    town: Joi.string().optional().allow(null),
-    region: Joi.string().optional().allow(null),
+    house_number: Joi.string().optional().allow(null, ''),
+    street_name: Joi.string().optional().allow(null, ''),
+    suburb: Joi.string().optional().allow(null, ''),
+    town: Joi.string().optional().allow(null, ''),
+    region: Joi.string().optional().allow(null, ''),
     outside_building: Joi.boolean()
       .truthy('true')
       .truthy('1')
